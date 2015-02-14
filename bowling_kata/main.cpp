26496100be40398e3338e7b3027c713e0e29de1a
Will Fisher <will@gina.alaska.edu>
2015-02-14 11:35:18 -0900

    initial commit

---
+++ bowling_kata/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bowling_kata
+//
+//  Created by Will Fisher on 2/14/15.
+//  Copyright (c) 2015 Will Fisher. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+
+TEST_CASE( "bowling kata") {
+}
